95914b8da72be7b2436afe9aa87283439e6772c8
young <pieangel11@naver.com>
2020-11-26 21:12:38 +0900

    Complete IntentTest

--- app/src/main/java/com/example/mirimintenttest/MainActivity.java
+++ app/src/main/java/com/example/mirimintenttest/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.mirimintenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnResult = findViewById(R.id.btn_result);
+        rg = findViewById(R.id.rg);
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
+
+        btnResult.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            intent.putExtra("input1", edit1.getText().toString());
+            intent.putExtra("input2", edit2.getText().toString());
+
+            String op = "";
+
+            switch (rg.getCheckedRadioButtonId()){
+                case R.id.radio_plus:
+                    op = "+";
+                    break;
+                case R.id.radio_minus:
+                    op = "-";
+                    break;
+                case R.id.radio_multi:
+                    op = "*";
+                    break;
+                case R.id.radio_divide:
+                    op = "/";
+                    break;
+            }
+
+            intent.putExtra("op", op);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/com/example/mirimintenttest/ResultActivity.java
+++ app/src/main/java/com/example/mirimintenttest/ResultActivity.java
@@ -2,13 +2,50 @@ package com.example.mirimintenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        TextView textResult = findViewById(R.id.text_result);
+
+        Intent intent = getIntent();
+        int input1 = Integer.parseInt(intent.getStringExtra("input1"));
+        int input2 = Integer.parseInt(intent.getStringExtra("input2"));
+
+        String op = intent.getStringExtra("op");
+
+        String result = "";
+
+        switch (op){
+            case "+":
+                result = input1 + " + " + input2 + " = " + (input1+input2);
+                break;
+            case "-":
+                result = input1 + " - " + input2 + " = " + (input1-input2);
+                break;
+            case "*":
+                result = input1 + " * " + input2 + " = " + (input1*input2);
+                break;
+            case "/":
+                result = input1 + " / " + input2 + " = " + (input1/input2);
+                break;
+        }
+
+        textResult.setText(result);
+
+        Button btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
